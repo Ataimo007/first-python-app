9f7d477b9320e3cb32a747ea46a70b6249262af9
Ataimo Edem <flexwitlex7@gmail.com>
2021-11-08 19:19:54 +0100

    First Python Application

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cmath
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def addition(x1, x2):
+    # Use a breakpoint in the code line below to debug your script.
+    return x1 + x2  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('World')
+    x1 = 10
+    x2 = 100
+    add = addition(x1, x2)
+    print(f'The total of {x1} and {x1}, is {add}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
